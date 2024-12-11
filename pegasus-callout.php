3cf7087db65926b20c935ac22374c37f6e622dbb
Jim O'Brien <jim.obrien3@gmail.com>
2024-12-11 01:08:32 -0500

    updating pegasus-callout plugin: usage page when not pegasus theme

--- pegasus-callout.php
+++ pegasus-callout.php
@@ -10,40 +10,138 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: wporg
 Domain Path: /languages
-*/
-	
+	*/
+
+	function callout_check_main_theme_name() {
+		$current_theme_slug = get_option('stylesheet'); // Slug of the current theme (child theme if used)
+		$parent_theme_slug = get_option('template');    // Slug of the parent theme (if a child theme is used)
+
+		//error_log( "current theme slug: " . $current_theme_slug );
+		//error_log( "parent theme slug: " . $parent_theme_slug );
+
+		if ( $current_theme_slug == 'pegasus' ) {
+			return 'Pegasus';
+		} elseif ( $current_theme_slug == 'pegasus-child' ) {
+			return 'Pegasus Child';
+		} else {
+			return 'Not Pegasus';
+		}
+	}
+
 	function pegasus_callout_menu_item() {
-		//add_menu_page("Callout", "Callout", "manage_options", "pegasus_callout_plugin_options", "pegasus_callout_plugin_settings_page", null, 99);
-		
+		if ( callout_check_main_theme_name() == 'Pegasus' || callout_check_main_theme_name() == 'Pegasus Child' ) {
+			//do nothing
+		} else {
+			//echo 'This is NOT the Pegasus theme';
+			add_menu_page(
+				"Callout", // Page title
+				"Callout", // Menu title
+				"manage_options", // Capability
+				"pegasus_callout_plugin_options", // Menu slug
+				"pegasus_callout_plugin_settings_page", // Callback function
+				null, // Icon
+				81 // Position in menu
+			);
+		}
 	}
 	add_action("admin_menu", "pegasus_callout_menu_item");
 
+	// function pegasus_callout_menu_item() {
+	// 	//add_menu_page("Callout", "Callout", "manage_options", "pegasus_callout_plugin_options", "pegasus_callout_plugin_settings_page", null, 99);
+
+	// }
+	// add_action(		"admin_menu", "pegasus_callout_menu_item");
+
 	function pegasus_callout_plugin_settings_page() { ?>
-	    <div class="wrap pegasus-wrap">
-	    <h1>Callout Usage</h1>
-		
-		<p>Callout Usage 1: <pre>[callout button="yes" link="http://example.com" external="yes" color="white" link_text="Learn More" background="http://www.wpfreeware.com/themes/html/appstation/img/download_bg.png"] <?php echo htmlspecialchars('<p>Get your copy now!Suspendisse vitae bibendum mauris. Nunc iaculis nisl vitae laoreet elementum donec dignissim metus sit.</p'); ?>[/callout]</pre></p>
-		<p>Callout Usage 2: <pre>[callout button="yes" link="http://example.com" color="black" external="yes" backgroundcolor="#dedede"] <?php echo htmlspecialchars('<h2>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</h2>'); ?>[/callout]</pre></p>
-			
-		<p style="color:red;">MAKE SURE YOU DO NOT HAVE ANY RETURNS OR <?php echo htmlspecialchars('<br>'); ?>'s IN YOUR SHORTCODES, OTHERWISE IT WILL NOT WORK CORRECTLY</p>
-		
-		
+
+		<div class="wrap pegasus-wrap">
+			<h1>Callout Usage</h1>
+<!--
+			<p>Callout Usage 1: <pre>[callout button="yes" link="http://example.com" external="yes" color="white" link_text="Learn More" background="http://www.wpfreeware.com/themes/html/appstation/img/download_bg.png"] <?php echo htmlspecialchars('<p>Get your copy now!Suspendisse vitae bibendum mauris. Nunc iaculis nisl vitae laoreet elementum donec dignissim metus sit.</p'); ?>[/callout]</pre></p>
+			<p>Callout Usage 2: <pre>[callout button="yes" link="http://example.com" color="black" external="yes" backgroundcolor="#dedede"] <?php //echo htmlspecialchars('<h2>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</h2>'); ?>[/callout]</pre></p>
+	-->
+			<div>
+				<h3>Callout Usage 1:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+
+					input[type="text"].code {
+						width: 100%;
+					}
+				</style>
+				<pre >[callout button="yes" link="http://example.com" external="yes" color="white" link_text="Learn More" background="http://www.wpfreeware.com/themes/html/appstation/img/download_bg.png"] <?php echo htmlspecialchars('<p>Get your copy now! Suspendisse vitae bibendum mauris. Nunc iaculis nisl vitae laoreet elementum donec dignissim metus sit.</p>'); ?>[/callout]</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[callout button="yes" link="http://example.com" external="yes" color="white" link_text="Learn More" background="http://www.wpfreeware.com/themes/html/appstation/img/download_bg.png"]<p>Get your copy now! Suspendisse vitae bibendum mauris. Nunc iaculis nisl vitae laoreet elementum donec dignissim metus sit.</p>[/callout]'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<div>
+				<h3>Callout Usage 2:</h3>
+				<style>
+					pre {
+						background-color: #f9f9f9;
+						border: 1px solid #aaa;
+						page-break-inside: avoid;
+						font-family: monospace;
+						font-size: 15px;
+						line-height: 1.6;
+						margin-bottom: 1.6em;
+						max-width: 100%;
+						overflow: auto;
+						padding: 1em 1.5em;
+						display: block;
+						word-wrap: break-word;
+					}
+				</style>
+				<pre >[callout button="yes" link="http://example.com" color="black" external="yes" backgroundcolor="#dedede"] <?php echo htmlspecialchars('<h2>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</h2>'); ?>[/callout]</pre>
+
+				<input
+					type="text"
+					readonly
+					value="<?php echo esc_html('[callout button="yes" link="http://example.com" color="black" external="yes" backgroundcolor="#dedede"]<h2>Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</h2>[/callout]'); ?>"
+					class="regular-text code"
+					id="my-shortcode"
+					onClick="this.select();"
+				>
+			</div>
+
+			<p style="color:red;">MAKE SURE YOU DO NOT HAVE ANY RETURNS OR <?php echo htmlspecialchars('<br>'); ?>'s IN YOUR SHORTCODES, OTHERWISE IT WILL NOT WORK CORRECTLY</p>
+
 		</div>
 	<?php
 	}
-	
+
 	function pegasus_callout_plugin_styles() {
 
 		wp_register_style( 'callout-plugin-css', trailingslashit( plugin_dir_url( __FILE__ ) ) . 'css/callout.css', array(), null, 'all' );
-		
+
 	}
 	add_action( 'wp_enqueue_scripts', 'pegasus_callout_plugin_styles' );
 
-	
+
 	/*~~~~~~~~~~~~~~~~~~~~
 		CALLOUT
 	~~~~~~~~~~~~~~~~~~~~~*/
-	
+
 	// [callout url="img-src"] text [/callout]
 	function pegasus_callout_func( $atts, $content = null ) {
 		$a = shortcode_atts( array(
@@ -55,39 +153,53 @@ Domain Path: /languages
 			'background' => '',
 			'backgroundcolor' => '',
 		), $atts );
-	
+
 		$output = '';
 		$color = "{$a['color']}";
 		$background = "{$a['background']}";
 		$bkg_color = "{$a['backgroundcolor']}";
-		if($background) { $output .= "<div class='callout_container' style='color: {$a['color']}; background: url({$a['background']});'>"; }
-		if($bkg_color) { $output .= "<div class='callout_container' style='color: {$a['color']}; background: {$a['backgroundcolor']};'>"; }
-		
+		$link = "{$a['link']}";
+		$button =  "{$a['button']}";
+		$external =  "{$a['external']}";
+		$link_text = "{$a['link_text']}";
+
+		if($background) {
+			$output .= "<div class='callout_container' style='color: {$a['color']}; background: url({$background});'>";
+		}
+		if($bkg_color) {
+			$output .= "<div class='callout_container' style='color: {$a['color']}; background: {$bkg_color};'>";
+		}
+
         $output .= "<div class=''>
                 <div class=''>
                   <div class='callout'>";
-                    
+
                     $output .= $content;
-					//$output .= '<a href ="http://example.com" class="btn btn-default"> Link Text </a>';
-					$button =  "{$a['button']}";
-					$external =  "{$a['external']}";
-					if($button === 'yes') { 
-						$output .= '<a href ="http://example.com" class="btn btn-default" ';
-						if($external === 'yes') { $output .= ' target="_blank" '; }
+					//$output .= '<a href ="http://example.com" class="                    btn btn-default"> Link Text </a>';
+
+					if($button === 'yes') {
+						$output .= '<a href ="' . $link . '" class="btn btn-default" ';
+						if($external === 'yes') {
+							$output .= ' target="_blank" ';
+						}
 						$output .= '>';
-						$link_text = "{$a['link_text']}";
-						if($link_text) { $output .= $link_text; }else{ $output .= 'Read More'; }
-						$output .= '</a>'; 
+
+						if($link_text) {
+							$output .= $link_text;
+						}else{
+							$output .= 'Read More';
+						}
+						$output .= '</a>';
 					}
-					
+
                 $output .= '</div>
                 </div>
               </div>
             </div>';
 
 		wp_enqueue_style( 'callout-plugin-css' );
-		
-		return $output; 
+
+		return $output;
 	}
 	add_shortcode( 'callout', 'pegasus_callout_func' );
-	
+
